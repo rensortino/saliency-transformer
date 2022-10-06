fc884d65f60b9ab547fa129798ce472393ec786d
Renato Sortino <rensortino@gmail.com>
2022-10-06 12:04:19 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-from evaluators.e_classifier import ClsEvaluator
 from utils.arg_parser import get_args_parser, setup
-from pathlib import Path
-from logzero import logger as lz_logger
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-from datamodules.base import BaseDataModule
-from models.m_classifier import Classifier
-from trainers.t_classifier import Trainer
 from utils.logging import Logger
-import json
-from utils.misc import count_parameters, fix_seed
-
-import torch
+from utils.misc import count_parameters, fix_seed, instantiate_from_config
 import wandb
-
+from omegaconf import OmegaConf
+from pathlib import Path
 def main(args):
 
     fix_seed(args.seed)
-    config = OmegaConf.load(args.config)
+    config = OmegaConf.load((Path('config') / args.config).with_suffix('.yaml'))
+
+    run = wandb.init(
+        mode="disabled" if args.debug else "online",
+        name=args.run_name,
+        config=args,
+    )
 
     # Data Loading
-    dm = BaseDataModule(data_dir='data')
+    dm = instantiate_from_config(config.data)
     train_loader = dm.train_dataloader(args.batch_size, args.device)
     if not args.one_batch:
         val_loader = dm.val_dataloader(args.batch_size, args.device)
         test_loader = dm.test_dataloader(args.batch_size, args.device)
     else:
-        lz_logger.warning('Training on a single batch')
+        logger.warning('Training on a single batch')
 
-
-    logger = Logger(args.output_dir, args.run_name)
+    logger = Logger(args.output_dir, args.run_name, run)
 
     trainer_config = config.trainer
     trainer_config['one_batch'] = args.one_batch
@@ -42,10 +38,10 @@ def main(args):
     #     scheduler = None
 
     print('*'*70)
-    lz_logger.info(f'Logging general information')
-    lz_logger.info(f'Debugging mode: {args.debug}')
-    lz_logger.info(f'Model total parameters {(count_parameters(trainer.model) / 1e6):.2f}M')
-    lz_logger.info(f'Model trainable parameters {(count_parameters(trainer.model, trainable=True) / 1e6):.2f}M')
+    logger.info(f'Logging general information')
+    logger.info(f'Debugging mode: {args.debug}')
+    logger.info(f'Model total parameters {(count_parameters(trainer.model) / 1e6):.2f}M')
+    logger.info(f'Model trainable parameters {(count_parameters(trainer.model, trainable=True) / 1e6):.2f}M')
     print('*'*70)
             
     try:
@@ -53,23 +49,23 @@ def main(args):
             assert args.resume, 'No weights selected for inference'
             epoch = args.start_epoch
             loss = evaluator.test_one_epoch(test_loader)
-            lz_logger.info(f'Inference loss: {loss}')
+            logger.info(f'Inference loss: {loss}')
             return
 
         # Training loop
         for epoch in range(trainer.start_epoch, args.epochs):
-            lz_logger.info(f'Training [{epoch} / {args.epochs}]')
+            logger.info(f'Training [{epoch} / {args.epochs}]')
             train_loss = trainer.train_one_epoch(train_loader)
             if (epoch + 1) % 10 == 0:
                 logger.save_ckpt(trainer.model.state_dict())
             logger.log_metric('train/loss', train_loss, epoch)
-            lz_logger.info(f'Epoch {epoch}: {train_loss}')
+            logger.info(f'Epoch {epoch}: {train_loss}')
             
             if args.log_weights:
-                writer.weight_histogram_adder(trainer.model.named_parameters(), epoch)
+                logger.weight_histogram_adder(trainer.model.named_parameters(), epoch)
 
             if args.log_gradients:
-                writer.gradient_histogram_adder(trainer.model.named_parameters(), epoch)
+                logger.gradient_histogram_adder(trainer.model.named_parameters(), epoch)
 
             if not (args.debug or args.one_batch):
                 ckpt = {
@@ -81,7 +77,7 @@ def main(args):
             if args.one_batch:
                 continue
 
-            lz_logger.info(f'Validation [{epoch} / {args.epochs}]')
+            logger.info(f'Validation [{epoch} / {args.epochs}]')
             val_loss = evaluator.validate_one_epoch(trainer.model, val_loader)
             logger.log_metric(f'val/loss', val_loss, epoch)
 
@@ -94,7 +90,7 @@ def main(args):
                 logger.save_ckpt(trainer.best_ckpt, 'best.pt')
 
             if (epoch + 1) % args.test_every_n_epochs == 0:
-                evaluator.test_one_epoch(test_loader)
+                evaluator.test_one_epoch(trainer.model, test_loader)
 
     except KeyboardInterrupt:
         print('Interrupted by user')
@@ -105,13 +101,5 @@ def main(args):
 if __name__ == '__main__':
 
     args = get_args_parser().parse_args()
-
-    run_mode = 'disabled' if args.debug else 'online'  
-    wandb.init(
-            config=args,
-            name=args.run_name,
-            mode=run_mode
-        )
-    
     setup(args)
     main(args)
